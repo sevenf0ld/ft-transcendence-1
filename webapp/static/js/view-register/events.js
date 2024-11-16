e766038d3cdee3b783f842ce12fa1f3a9d656a40
liaw-dev <149603557+liaw-dev@users.noreply.github.com>
2024-11-16 23:53:05 +0800

    WIP : Refactor project (10%)
        - login-page (bare-bone)
        - register-page (bare-bone)

---
+++ webapp/static/js/view-register/events.js
@@ -0,0 +1,57 @@
+// file : events.js (view-register)
+// -------------------------------------------------- //
+// Importing-internal
+// -------------------------------------------------- //
+// -------------------------------------------------- //
+// Importing-external
+// -------------------------------------------------- //
+import * as MEDIA from '../utils/media.js';
+import * as COOKIE from '../utils/cookie.js';
+import * as FORGETPASS from '../view-forgetpass/template.js';
+import * as HOME from '../view-home/template.js';
+import * as LIVECHAT from '../view-livechat/template.js';
+import * as LOCALPVE from '../view-local-pve/template.js';
+import * as LOCALPVP from '../view-local-pvp/template.js';
+import * as LOCALTOUR from '../view-local-tour/template.js';
+import * as LOGIN from '../view-login/template.js';
+import * as PROFILE from '../view-profile/template.js';
+import * as REGISTER from '../view-register/template.js';
+import * as REMOPVP from '../view-remo-pvp/template.js';
+import * as REMOTOUR from '../view-remo-tour/template.js';
+// -------------------------------------------------- //
+// developer notes
+// -------------------------------------------------- //
+async function register_btn(obj)
+{
+	obj.addEventListener('click', async (event) => {
+		console.log('register-button clicked');
+	});
+
+	return true;
+}
+
+async function back_to_login_btn(obj)
+{
+	obj.addEventListener('click', async (event) => {
+		console.log('back-to-login-button clicked');
+		LOGIN.build();
+	});
+
+	return true;
+}
+
+async function build()
+{
+	const btn = document.getElementById('btn_register');
+	const btn2 = document.getElementById('btn_back_to_login');
+
+	await register_btn(btn);
+	await back_to_login_btn(btn2);
+}
+
+// -------------------------------------------------- //
+// export
+// -------------------------------------------------- //
+export {
+	build
+};
