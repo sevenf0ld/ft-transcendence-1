7ab8f61c7125fafd84c0a8e9100bd8833df8969b
sevenf0ld <115096194+sevenf0ld@users.noreply.github.com>
2024-12-22 23:23:33 +0800

    add api and backend login for email, password and 2fa update for user account
    * related to issue #77
    
    - note:
        - frontend to call the api and implement frontend form validation

--- requirements/django/webapp/user_auth/utils.py
+++ requirements/django/webapp/user_auth/utils.py
@@ -1,5 +1,7 @@
 from user_profiles.models import Profile
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.password_validation import validate_password
 
 def is_mfa_enabled(request):
     username = request.data.get('username')
@@ -7,3 +9,36 @@ def is_mfa_enabled(request):
     profile_data = Profile.objects.get(user=user_data)
 
     return profile_data.mfa_email_enabled
+
+def is_old_email(user, email):
+    user_data = User.objects.get(user=user)
+    if user_data.email.lower() == email:
+        return True
+    return False
+
+def is_existing_email(email):
+    if User.objects.filter(email__iexact=email).exists():
+        return True
+    return False
+
+def is_old_password(user, password):
+    if check_password(password, user.password):
+        return True
+    return False
+
+def is_valid_password(user, password):
+    try:
+        if validate_password(password, user) is None:
+            return None
+    except ValidationError as e:
+        return e.messages
+
+def enable_mfa(user):
+    profile_data = Profile.objects.get(user=user_data)
+    profile_data.mfa_email_enabled = True
+    profile_data.save()
+
+def disable_mfa(user):
+    profile_data = Profile.objects.get(user=user_data)
+    profile_data.mfa_email_enabled = False
+    profile_data.save()
