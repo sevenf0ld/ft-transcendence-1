0be9b80e854a0da90c9288c0d3c0da94cb0bbf00
sevenf0ld <115096194+sevenf0ld@users.noreply.github.com>
2024-12-22 08:04:34 +0000

    handle user leaving and joining private chat room (#76)
    * only authenticated users can join private chat (2 person max)
    * authentication enforced via middleware
    * leaving via close chatbox symbol closes websocket
    * both users will be notified when the friend leaves or joins the chat
    room via console log
    
    frontend to continue development as per issue #74

--- requirements/django/webapp/chat/consumers.py
+++ requirements/django/webapp/chat/consumers.py
@@ -2,59 +2,145 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
+MAX_PRIVATE_CHAT_USERS = 2
+
+# maybe change everything to 4k+
 class ChatConsumer(WebsocketConsumer):
+    in_room = {}
+
+    #========================================#
+    #========== GETS CONNECTED TO ===========#
+    #========================================#
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
+        if self.room_group_name not in self.in_room:
+            self.in_room[self.room_group_name] = set()
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
-        self.accept()
+        user = self.scope['user']
+        if user.is_authenticated:
+            if user.username in self.in_room[self.room_group_name]:
+                self.accept()
 
+                in_room_users = len(self.in_room[self.room_group_name])
+                self.send(text_data=json.dumps({
+                    'type': 'reconnect',
+                    'message': f"{user} has reconnected to {self.room_group_name}.",
+                    'num_in_room': in_room_users
+                }))
+
+                if in_room_users == 2:
+                    async_to_sync(self.channel_layer.group_send)(
+                        self.room_group_name,
+                        {
+                            'type': 'chat.ready'
+                        }
+                    )
+            elif len(self.in_room[self.room_group_name]) < MAX_PRIVATE_CHAT_USERS:
+                self.accept()
+
+                self.in_room[self.room_group_name].add(user.username)
+
+                async_to_sync(self.channel_layer.group_add)(
+                    self.room_group_name,
+                    self.channel_name
+                )
+
+                in_room_users = len(self.in_room[self.room_group_name])
+                self.send(text_data=json.dumps({
+                    'type': 'connect',
+                    'message': f"{user} is now connected to {self.room_group_name}.",
+                    'num_in_room': in_room_users
+                }))
+
+                if in_room_users == 2:
+                    async_to_sync(self.channel_layer.group_send)(
+                        self.room_group_name,
+                        {
+                            'type': 'chat.ready'
+                        }
+                    )
+            else:
+                self.close(3003)
+        else:
+            self.close(3000)
+
+    def chat_ready(self, event):
         self.send(text_data=json.dumps({
-            'type': 'connection_established',
-            'message': f"{self.scope['user']} is now connected to {self.room_group_name}."
+            'type': 'chat_ready'
         }))
 
+    #========================================#
+    #========== DISCONNECTED FROM ===========#
+    #========================================#
     def disconnect(self, code):
+        user = self.scope['user']
+
+        if self.room_group_name in self.in_room:
+            self.in_room[self.room_group_name].discard(user.username)
+            if not self.in_room[self.room_group_name]:
+                del self.in_room[self.room_group_name]
+
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
         self.send(text_data=json.dumps({
-            'type': 'connection_closed',
-            'message': f"{self.scope['user']} has disconnected from {self.scope['room_group_name']}."
+            'type': 'disconnect',
+            'message': f"{self.scope['user']} has disconnected from {self.room_group_name}.",
         }))
 
-
+    #========================================#
+    #========== RECEIVE A MESSAGE ===========#
+    #============== BROADCAST ===============#
+    #========================================#
     def receive(self, text_data):
         text_json = json.loads(text_data)
         message = text_json['message']
         sender = text_json['sender']
+        message_type = text_json['type']
 
-        print(self.scope['path']) # /ws/chat/
-        print(self.scope['raw_path']) # b'/ws/chat/
-        print(self.scope['user']) # maiman-m
-        print(self.scope['url_route']) # {'args': (), 'kwargs': {}}
+        if message_type == 'chat_reply':
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat.reply',
+                    'message': message,
+                    'sender': sender
+                }
+            )
+        elif message_type == 'chat_close':
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat.close',
+                    'message': f'{sender} has left the room.',
+                }
+            )
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'reply.chat',
-                'message': message,
-                'sender': sender
-            }
-        )
-
-    def reply_chat(self, event):
+    #========================================#
+    #========== RECEIVE A MESSAGE ===========#
+    #============== CHATTING ================#
+    #========================================#
+    def chat_reply(self, event):
         message = event['message']
         sender = event['sender']
 
         self.send(text_data=json.dumps({
-            'type': 'message_received',
+            'type': 'chat_reply',
             'message': message,
             'sender': sender
+        }))
+
+    #========================================#
+    #========== RECEIVE A MESSAGE ===========#
+    #============ NOTIFY CLIENT =============#
+    #========================================#
+    def chat_close(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
+            'type': 'chat_close',
+            'message': message,
         }))
