b8c7ecdcfd1ea9bc9698735679ddc481e657c0ad
sevenf0ld <115096194+sevenf0ld@users.noreply.github.com>
2024-12-27 19:27:57 +0000

    add api for viewing match histories (#94)

--- requirements/django/webapp/games/serializers.py
+++ requirements/django/webapp/games/serializers.py
@@ -1,18 +1,72 @@
-from rest_framework import serializers 
-  
+from rest_framework import serializers
+
 from .models import Match, Tournament, GameHistory
-  
-class MatchModelSerializer(serializers.ModelSerializer): 
-    class Meta: 
+
+class MatchModelSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+    game_type = serializers.SerializerMethodField()
+    opponent = serializers.SerializerMethodField()
+
+    def get_status(self, obj):
+        user = obj.user.username
+        winner = obj.match_winner.user.username
+        if user == winner:
+            return 'WON'
+        return 'LOST'
+
+    def get_game_type(self, obj):
+        return 'PVP'
+
+    def get_opponent(self, obj):
+        user = obj.user.username
+        p1 = obj.p1.username
+        if user == p1:
+            return obj.p2.username
+        return p1
+
+    class Meta:
         model = Match
-        fields = '__all__'
-  
-class TournamentModelSerializer(serializers.ModelSerializer): 
-    class Meta: 
+        fields = ['match_time', 'status', 'game_type', 'opponent']
+
+class TournamentModelSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+    game_type = serializers.SerializerMethodField()
+    opponent = serializers.SerializerMethodField()
+
+    def get_status(self, obj):
+        user = obj.user.username
+        winner = obj.tnm_winner.user.username
+        if user == winner:
+            return 'WON'
+        return 'LOST'
+
+    def get_game_type(self, obj):
+        return 'TNM'
+
+    def get_opponent(self, obj):
+        user = obj.user.username
+        winner = obj.tnm_winner.user.username
+        if user == winner:
+            return obj.tnm_host.username
+        return winner
+
+    class Meta:
         model = Tournament
-        fields = '__all__'
-  
-class GameHistoryModelSerializer(serializers.ModelSerializer): 
-    class Meta: 
+        fields = ['tnm_time', 'status', 'game_type', 'opponent']
+
+# date (time), status (lost/win), type, opponent (winner/loser)
+class GameHistoryModelSerializer(serializers.ModelSerializer):
+    pvp = serializers.SerializerMethodField()
+    tnm = serializers.SerializerMethodField()
+
+    def get_pvp(self, obj):
+        pvp = obj.matches.all()
+        return MatchModelSerializer(pvp, many=True).data
+
+    def get_tnm(self, obj):
+        tnm = obj.tournaments.all()
+        return TournamentModelSerializer(tnm, many=True).data
+
+    class Meta:
         model = GameHistory
-        fields = '__all__'
+        fields = ['pvp', 'tnm']
--- requirements/django/webapp/games/views.py
+++ requirements/django/webapp/games/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
-from .models import Match, Tournament, GameHistory
-from .serializers import MatchModelSerializer, TournamentModelSerializer, GameHistoryModelSerializer
+#from rest_framework import viewsets
+from .models import GameHistory
+from .serializers import GameHistoryModelSerializer
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import AllowAny
+from dj_rest_auth.jwt_auth import JWTCookieAuthentication
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status, generics
+from typing import Dict, Any
 
-class MatchViewSet(viewsets.ModelViewSet):
-    serializer_class = MatchModelSerializer
-    queryset = Match.objects.all()
+#class MatchViewSet(viewsets.ModelViewSet):
+#    serializer_class = MatchModelSerializer
+#    queryset = Match.objects.all()
+#
+#class TournamentViewSet(viewsets.ModelViewSet):
+#    serializer_class = TournamentModelSerializer
+#    queryset = Tournament.objects.all()
+#
+#class GameHistoryViewSet(viewsets.ModelViewSet):
+#    serializer_class = GameHistoryModelSerializer
+#    queryset = GameHistory.objects.all()
 
-class TournamentViewSet(viewsets.ModelViewSet):
-    serializer_class = TournamentModelSerializer
-    queryset = Tournament.objects.all()
-
-class GameHistoryViewSet(viewsets.ModelViewSet):
+class GameHistoryRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = GameHistory.objects.prefetch_related('matches', 'tournaments')
     serializer_class = GameHistoryModelSerializer
-    queryset = GameHistory.objects.all()
+    lookup_field = 'user__username'
+    lookup_url_kwarg = 'target'
