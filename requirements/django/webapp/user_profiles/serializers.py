7e99e22e664469e3aee4f2b4c4d773d4bdd90db0
sevenf0ld <115096194+sevenf0ld@users.noreply.github.com>
2024-12-27 14:53:46 +0800

    add profile related info to user login details for HomeView
    * avatar and languages not sent yet
    
    - note:
        - modify frontend to work with the info

--- requirements/django/webapp/user_profiles/serializers.py
+++ requirements/django/webapp/user_profiles/serializers.py
@@ -2,9 +2,29 @@ from rest_framework import serializers
 
 from .models import Profile
 
-class ProfileModelSerializer(serializers.ModelSerializer): 
+class ProfileModelSerializer(serializers.ModelSerializer):
+    played = serializers.SerializerMethodField()
+    win_rate = serializers.SerializerMethodField()
+
+    def get_played(self, obj):
+        total_matches = obj.wins + obj.losses
+        return total_matches
+
+    def get_win_rate(self, obj):
+        total_matches = obj.wins + obj.losses
+        if total_matches == 0:
+            return 0
+        return obj.wins / total_matches * 100
+
     class Meta:
         model = Profile
+        # avatar and language to be added
+        fields = ['played', 'win_rate', 'wins', 'losses', 'nickname', 'mfa_email_enabled']
+
+class FriendProfileModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        # avatar to add
         fields = '__all__'
 
 class UploadAvatarSerializer(serializers.ModelSerializer):
