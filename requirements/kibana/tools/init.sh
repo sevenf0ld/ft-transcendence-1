5721409fb502ece93efbf1eeb776a16939234861
sevenf0ld <115096194+sevenf0ld@users.noreply.github.com>
2025-01-14 06:14:22 +0800

    configure elastisearch to retain data through kibana

--- requirements/kibana/tools/init.sh
+++ requirements/kibana/tools/init.sh
@@ -43,8 +43,85 @@ fi
 
 # https://www.elastic.co/guide/en/kibana/7.17/saved-objects-api-export.html
 # https://www.elastic.co/guide/en/kibana/7.17/saved-objects-api-import.html
-# visualization
-# dashboard
 curl -X POST "http://kibana:5601/api/saved_objects/_import?createNewCopies=true" \
 	-H "kbn-xsrf: true" \
 	--form file=@"/usr/share/kibana/saved_objects/dashboard.ndjson"
+
+# https://www.elastic.co/guide/en/elasticsearch/reference/7.14/getting-started-index-lifecycle-management.html
+curl -X PUT "http://elasticsearch:9200/_ilm/policy/logs_policy?pretty" \
+	-H "Content-Type: application/json" \
+	-d'
+	{
+	  "policy": {
+	    "phases": {
+	      "hot": {
+	        "actions": {
+	          "rollover": {
+	            "max_primary_shard_size": "500MB",
+	            "max_age": "10d"
+	          }
+	        }
+	      },
+	      "delete": {
+	        "min_age": "20d",
+	        "actions": {
+	          "delete": {}
+	        }
+	      }
+	    }
+	  }
+	}
+	'
+
+#curl -X PUT "http://elasticsearch:9200/_index_template/timeseries_template?pretty" \
+#	-H "Content-Type: application/json" \
+#	-d'
+#	{
+#	  "index_patterns": ["timeseries"],
+#	  "data_stream": { },
+#	  "template": {
+#	    "settings": {
+#	      "number_of_shards": 1,
+#	      "number_of_replicas": 1,
+#	      "index.lifecycle.name": "timeseries_policy"
+#	    }
+#	  }
+#	}
+#	'
+
+#curl -X POST "http://elasticsearch:9200/timeseries/_doc?pretty" \
+#	-H "Content-Type: application/json" \
+#	-d'
+#	{
+#	  "message": "logged the request",
+#	  "@timestamp": "1591890611"
+#	}
+#	'
+
+curl -X PUT "http://elasticsearch:9200/_index_template/logs_template?pretty" \
+	-H "Content-Type: application/json" \
+	-d'
+	{
+	  "index_patterns": ["logs-*"],
+	  "template": {
+	    "settings": {
+	      "number_of_shards": 1,
+	      "number_of_replicas": 1,
+	      "index.lifecycle.name": "logs_policy",
+	      "index.lifecycle.rollover_alias": "logs"
+	    }
+	  }
+	}
+	'
+
+curl -X PUT "http://elasticsearch:9200/logs-000001?pretty" \
+	-H "Content-Type: application/json" \
+	-d'
+	{
+	  "aliases": {
+	    "logs": {
+	      "is_write_index": true
+	    }
+	  }
+	}
+	'
