995c15e881096ba0a94933d7900f8ef08f108cc9
sevenf0ld <115096194+sevenf0ld@users.noreply.github.com>
2025-01-15 17:59:44 +0800

    configuration for ngrok

--- requirements/django/webapp/pong/settings.py
+++ requirements/django/webapp/pong/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ftpong.com', 'api.ftpong.com', '0.0.0.0']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ftpong.com', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ftpong.com', '0.0.0.0', '*']
 #config('DJANGO_ALLOWED_HOSTS')
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
